0c4010e307253418b05c057c399a02e0422de1ca
kwalkerxxi <batteryyrettab@hotmail.com>
2026-06-13 23:17:18 +1000

    Improvements to Background Trucks

---
+++ Assets/Asteroids/_fixed-to-re-allocate-20260530/improved-truck-rendering/CityTrafficInstanced.cs
@@ -0,0 +1,202 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Splines;
+
+namespace Riley.Scripts
+{
+    public class CityTrafficInstanced : MonoBehaviour
+    {
+        [SerializeField] private SplineContainer[] truckSplinePaths;
+
+        [Header("Rendering")]
+        [SerializeField] private Mesh truckMesh;
+        [SerializeField] private Material truckMaterial;
+        [SerializeField] Material[] truckMaterials;
+        [SerializeField] private float truckScale = 1f;
+
+        [Header("Traffic")]
+        [SerializeField] private int spawnCount = 10;
+        [SerializeField] private float truckSpeed = 5f;
+
+        struct Truck
+        {
+            public SplineContainer spline;
+            public float normalizedPosition;
+            public float splineLength;
+        }
+
+        private Truck[] trucks;
+        private Matrix4x4[] matrices;
+
+        //private RenderParams renderParams;
+
+        [SerializeField]
+        private RenderParams[] submeshParams;
+
+        [SerializeField]
+        private int renderLayer = 11;
+        [SerializeField] private string renderLayerName = "Buildings";
+
+        void Start()
+        {
+            if(truckSplinePaths.Length <= 0)
+            {
+                truckSplinePaths = GetComponentsInChildren<SplineContainer>();
+            }
+
+
+            InitializeTraffic();
+
+            //renderParams = new RenderParams(truckMaterial)
+            //{
+            //    shadowCastingMode = ShadowCastingMode.On,
+            //    receiveShadows = true,
+            //    layer = gameObject.layer
+            //};
+
+            submeshParams = new RenderParams[truckMesh.subMeshCount];
+
+            for(int i = 0; i < truckMesh.subMeshCount; i++)
+            {
+                submeshParams[i] = new RenderParams(truckMaterials[i])
+                {
+                    shadowCastingMode = ShadowCastingMode.Off,
+                    receiveShadows = false,
+                    layer = LayerMask.NameToLayer(renderLayerName)
+                };
+            }
+        }
+
+        void InitializeTraffic()
+        {
+            int totalTrucks = truckSplinePaths.Length * spawnCount;
+
+            trucks = new Truck[totalTrucks];
+            matrices = new Matrix4x4[totalTrucks];
+
+            int index = 0;
+
+            foreach(SplineContainer spline in truckSplinePaths)
+            {
+                float length = spline.CalculateLength();
+                float spacing = length / (spawnCount - 1);
+
+                for(int i = 0; i < spawnCount; i++)
+                {
+                    float distance = spacing * i;
+
+                    float t =
+                        spline.Spline.ConvertIndexUnit(
+                            distance,
+                            PathIndexUnit.Distance,
+                            PathIndexUnit.Normalized);
+
+                    trucks[index] = new Truck
+                    {
+                        spline = spline,
+                        normalizedPosition = t,
+                        splineLength = length
+                    };
+
+                    index++;
+                }
+            }
+        }
+
+        void Update()
+        {
+            UpdateTraffic();
+
+            //Graphics.RenderMeshInstanced(
+            //    renderParams,
+            //    truckMesh,
+            //    0,
+            //    matrices,
+            //    matrices.Length);
+
+            //for(int submesh = 0; submesh < truckMesh.subMeshCount; submesh++)
+            //{
+            //    Graphics.RenderMeshInstanced(
+            //        renderParams,
+            //        truckMesh,
+            //        submesh,
+            //        matrices,
+            //        matrices.Length);
+            //}
+
+            //for(int submesh = 0; submesh < truckMesh.subMeshCount; submesh++)
+            //{
+            //    //renderParams.material = truckMaterials[submesh];
+            //    Material materialToUse = truckMaterials[submesh];
+            //    renderParams.material = materialToUse;
+            //    renderParams = new RenderParams(materialToUse)
+            //    {
+            //        shadowCastingMode = ShadowCastingMode.On,
+            //        receiveShadows = true,
+            //        layer = renderLayer
+            //    };
+
+            //    renderParams.layer = LayerMask.NameToLayer(renderLayerName);
+
+            //    Graphics.RenderMeshInstanced(
+            //        renderParams,
+            //        truckMesh,
+            //        submesh,
+            //        matrices,
+            //        matrices.Length);
+            //}
+
+            for(int submesh = 0; submesh < truckMesh.subMeshCount; submesh++)
+            {
+                Graphics.RenderMeshInstanced(
+                    submeshParams[submesh],
+                    truckMesh,
+                    submesh,
+                    matrices,
+                    matrices.Length
+                );
+            }
+        }
+
+
+
+        void UpdateTraffic()
+        {
+            for(int i = 0; i < trucks.Length; i++)
+            {
+                Truck truck = trucks[i];
+
+                truck.normalizedPosition +=
+                    (truckSpeed / truck.splineLength) *
+                    Time.deltaTime;
+
+                truck.normalizedPosition %= 1f;
+
+                Vector3 position =
+                    truck.spline.EvaluatePosition(
+                        truck.normalizedPosition);
+
+                Vector3 tangent =
+                    truck.spline.EvaluateTangent(
+                        truck.normalizedPosition);
+
+                Quaternion rotation =
+                    tangent.sqrMagnitude > 0.0001f
+                        ? Quaternion.LookRotation(tangent)
+                        : Quaternion.identity;
+
+                //matrices[i] = Matrix4x4.TRS(
+                //    position,
+                //    rotation,
+                //    Vector3.one);
+
+                matrices[i] = Matrix4x4.TRS(
+                    position,
+                    rotation,
+                    Vector3.one * truckScale);
+
+                trucks[i] = truck;
+            }
+        }
+    }
+}
